dba4543f441f514b9fa007055dc1c68f90841714
Alex Ware <alex.ware@gmail.com>
2020-11-30 22:24:37 +0000

    sleeep

--- jobs/sleep.js
+++ jobs/sleep.js
@@ -1,10 +1,9 @@
 import { getSwitch, button } from '../tp.js'
-import { getLights } from '../lifx.js'
  import Lifx from "node-lifx-lan";
 
 const sunset = [{ "h": 0.05687203791469195, "s": 0.8902953586497889, "l": 0.5352941176470588 }, { "h": 0.9894636015325671, "s": 0.7631578947368421, "l": 0.44705882352941173 }, { "h": 0.9054373522458629, "s": 0.8245614035087719, "l": 0.33529411764705885 }, { "h": 0.7966101694915254, "s": 0.9365079365079365, "l": 0.24705882352941178 }, { "h": 0.6994535519125683, "s": 0.7530864197530865, "l": 0.1588235294117647 }]
-const cycle = 1000 * 60 * 10
-const fade = 1000 * 60 
+const cycle = 1000 * 10//* 60 * 10
+const fade = 1000 //* 60 
 
 const wait = (t) => new Promise(resolve => {
   setTimeout(()=>resolve(),t)
@@ -13,32 +12,40 @@ const wait = (t) => new Promise(resolve => {
 let temp = 2500
 
 const run = async () => {
+  console.log("TIME TO SLEEP")
   const s = await getSwitch()
   s.power(true)
-  const ls = await getLights()
-
   const { onChange, hasChanged } = await button(s)
   onChange(async () => {
+    console.log("TURN IT OFFF")
     await Lifx.turnOffBroadcast({ duration: 4000 })
+    await wait(6000)
     process.exit(0)
   })
-  console.log(Object.keys(ls))
-  let b = 100
+  let b = 1
   let n = 12
-  ls.lemp.on()
-  ls.lomp.on()
+  Lifx.turnOnBroadcast()
   while (n > 0) {
     if (hasChanged()) {
       process.exit(0)
     }
-    const coloura = sunset[~~(Math.random() * sunset.length)]
-    const colourb = sunset[~~(Math.random() * sunset.length)]
+    const devices = await Lifx.discover()
+    devices.map(lamp => {
+    const colour = sunset[~~(Math.random() * sunset.length)]
+      console.log(lamp.deviceInfo.label,colour)
+      lamp.setColor({
+        color: {
+          hue: colour.h,
+          saturation: colour.s,
+          brightness: b,
+          kelvin: temp
+        },
+        duration: fade
+      })
+    })
 
-    ls.lemp.color(coloura.h * 360, coloura.s * 100, b, temp, fade)
-    
-    ls.lomp.color(colourb.h * 360, colourb.s * 100, b, temp, fade)
+   console.log("CYCLE")
     b = b * 0.7
-    console.log(b)
     n--
     await wait(cycle)
   }
